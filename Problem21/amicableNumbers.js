f0c190a09fa77df4e441e6b5a1f4888ff944a13a
user <user@users-Air-2.fios-router.home>
2019-02-12 16:03:28 -0500

    solved problem 21

---
+++ Problem21/amicableNumbers.js
@@ -0,0 +1,18 @@
+function sumOfDivisors(x){
+    let total = 0;
+    for(let i = 1; i < x;i++){
+	if(x % i === 0)
+	    total += i;
+    }
+    return total;
+}
+let answer = 0;
+for(let a = 1; a < 10000; a++){
+    let b = sumOfDivisors(a);
+    let bSum = sumOfDivisors(b);
+    if(a === bSum)
+	if(a !== b){
+	    answer += a;
+	}
+}
+console.log("The answer is: "  + answer);
